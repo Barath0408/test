76b62d32604b1f4ca23af436ef89826383010041
Barath0408 <barath04crazy@gmail.com>
2024-03-27 12:59:45 +0530

    4th assignment

---
+++ DAY5/index.js
@@ -0,0 +1,6 @@
+let firstname = "Barath";
+let lastname = "N";
+let city = " Trichy";
+let age = "23";
+
+console.log(`MY name is ${firstname} ${lastname}. i am currently live in ${city}. my age is ${age}` );
--- Day 4/index.js
+++ Day 4/index.js
@@ -1,40 +1,34 @@
-var obj1 = { name: "personal1", age: 5 };
-var obj2 = { age: 5, name: "personal2" };
 
-function areObjectequal(obj1, obj2) {
-  var keys1 = Object.keys(obj1);
-  var keys2 = Object.keys(obj2);
 
-  if (keys1.length !== keys2.length) {
-    return false;
-  }
+function addTwoNumber(a, b) {
 
-  for (var key of keys1) {
-    if (obj1[key] !== obj2[key]) {
-      return false;
-    }
-  }
-  return true;
+  var c = a + b;
+  return c;
+}
+var result = addTwoNumber(10, 125);
+console.log(result);
+
+function applyDiscount(discount) {
+  var cartamount = 1000;
+  cartamount -= discount;
+  console.log(cartamount);
 }
 
-console.log(areObjectequal(obj1, obj2));
+applyDiscount(500);
 
-var xhr = new XMLHttpRequest(); //CREAT A OBJECT
-
-xhr.open("GET", "https://restcountries.com/v3.1/all", true);
-
-xhr.onload = function () {
-  //console.log("XMLHttpRequest- status", xhr.status);
-  var countries = JSON.parse(xhr.responseText);
-  console.log(countries);
-  //  countries.forEach((countries) => {
-  //  console.log(countries.name.common + ":" + countries.flag + ":" + countries.region.subregion+":"+ countries.populations);
-  // });
-  for (var i = 0; i < countries.length; i++) {
-    console.log(countries[i].name.common + ":" + countries[i].flag);
-    console.log("populations:", countries[i].population);
-    console.log("regions:", countries[i].region);
-    console.log("subregion:", countries[i].subregion);
-  }
-};
-xhr.send();
+(function (a, b) {
+  console.log("this is iife", a + b)
+  
+})(10, 30); 
+//(function anonymous)(arguments);
+  
+var array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+function(array) {
+    for (var i = 0; i < array.lenghth; i++){
+    
+        if (array[i] % 1= 0) {
+            console.log(array[i]);
+        }
+}   
+}
+(array);
